170f5b5895a5391109a6352d23dc75804c1740df
Gabriel <272199@student.calderdale.ac.uk>
2024-10-22 20:35:29 +0100

    Add trend functionality using linear regression to measure the slope.

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const url = "https://io.adafruit.com/api/v2/calderdigihub/feeds/course-plant-moi
 function getMean(dataArray) {
   let total = 0;
   let count = dataArray.length;
-  for (let data in dataArray) {
-    total += data;
+  for (const data of dataArray) {
+    total += parseFloat(data);
   }
 
   return total / count;
@@ -14,8 +14,8 @@ function getArrays(moistureData) {
   let moisture = [];
   let unixTime = [];
   for (const element of moistureData) {
-    moisture.push(element.value);
-    unixTime.push(element.created_at);
+    moisture.push(parseFloat(element.value));
+    unixTime.push(parseFloat(new Date(element.created_at).getTime()));
   };
 
   return [moisture, unixTime];
@@ -37,14 +37,30 @@ function moistureLinReg(moistureData) {
   let slope = numerator / denominator;
   let intercept = avgMoisture - (slope * avgUnixTime);
 
-  return { slope, intercept };
+  return [slope, intercept];
+}
+
+function isMoistureOverTimeStable(moistureData) {
+  let slope = moistureLinReg(moistureData)[0]
+  if (slope <= 0.1 && slope >= -0.1) {
+    return "Stable";
+  }
+  else if (slope < -0.1) {
+    return "Negative";
+  }
+  else if (slope > 0.1) {
+    return "Positive"
+  }
+  else return "Should not be returning this (something went wrong)"
 }
 
 fetch(url)
   .then(response => response.json())
   .then(data => {
     const latestMoisture = data[0].value;
+    const dataTrend = isMoistureOverTimeStable(data)
     document.getElementById("moistureData").innerHTML = `<h2>Current Moisture: ${latestMoisture}</h2>`;
+    document.getElementById("stability").innerHTML = `<h2>Trend: ${dataTrend}</h2>`
   })
   .catch(error => {
     document.getElementById("moistureData").innerHTML = `<p style="color:red;">Error fetching data: ${error.message}</p>`;
