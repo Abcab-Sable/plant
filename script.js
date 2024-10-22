c0eb308d0abf4db0a52debb279c8f06dcea6f252
Gabriel <272199@student.calderdale.ac.uk>
2024-10-22 20:17:22 +0100

    Perform linear regression calculation on the moisture data

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
 const url = "https://io.adafruit.com/api/v2/calderdigihub/feeds/course-plant-moisture/data/";
 
+function getMean(dataArray) {
+  let total = 0;
+  let count = dataArray.length;
+  for (let data in dataArray) {
+    total += data;
+  }
+
+  return total / count;
+}
+
+function getArrays(moistureData) {
+  let moisture = [];
+  let unixTime = [];
+  for (const element of moistureData) {
+    moisture.push(element.value);
+    unixTime.push(element.created_at);
+  };
+
+  return [moisture, unixTime];
+}
+
+function moistureLinReg(moistureData) {
+  let moisture = getArrays(moistureData)[0];
+  let unixTime = getArrays(moistureData)[1];
+  let avgMoisture = getMean(moisture);
+  let avgUnixTime = getMean(unixTime);
+
+  let numerator = 0;
+  let denominator = 0;
+  for (let i = 0; i < moisture.length; i++) {
+    numerator += ((unixTime[i] - avgUnixTime) * (moisture[i] - avgMoisture));
+    denominator += (unixTime[i] - avgUnixTime) ** 2;
+  };
+
+  let slope = numerator / denominator;
+  let intercept = avgMoisture - (slope * avgUnixTime);
+
+  return { slope, intercept };
+}
+
 fetch(url)
   .then(response => response.json())
   .then(data => {
